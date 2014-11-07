b676e20781652f7ef3b738678416e491b8b684f3
proll <gleb.proll@gmail.com>
2014-11-07 19:55:19 +0300

    a try of new product popup

--- scripts/app.js
+++ scripts/app.js
@@ -105,10 +105,9 @@ $(function() {
 
 			jqxhr = 
 				$.when.apply($, ajax_objects)
-					.done(function(results) {
-						console.log(results)
+					.done(function() {
+						console.log(arguments)
 						_.forEach(results, function(result) {
-							console.log(result)
 							$slides_cont.find('.overlay__slides').html(result[0]);
 							$slides = $slides_cont.find('.overlay__slide');
 

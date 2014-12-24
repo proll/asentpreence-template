2388c69a7b88f253e6d87c4324bd9eea2cf8d383
proll <gleb.proll@gmail.com>
2014-12-24 11:58:02 +0300

    js test

--- scripts/app.js
+++ scripts/app.js
@@ -108,6 +108,7 @@ $(function() {
 				$.when.apply($, ajax_objects)
 					.done(function() {
 						_.forEach(arguments, function(result) {
+							console.log(result)
 							$slides_cont.find('.overlay__slides').append(result[0]);
 							$slides = $slides_cont.find('.overlay__slide');
 
